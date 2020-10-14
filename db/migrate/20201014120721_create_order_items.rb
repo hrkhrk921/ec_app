98dd6eac4bdc15d5da423eb29554355d3e5b7f1f
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-10-14 21:11:26 +0900

    Order_itemモデル作成

--- db/migrate/20201014120721_create_order_items.rb
+++ db/migrate/20201014120721_create_order_items.rb
@@ -1,6 +1,11 @@
 class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
+      t.integer :order_id, null: false
+      t.integer :item_id, null: false
+      t.integer :purchase_price, null: false
+      t.integer :quantity, null: false
+      t.integer :status, null: false, default: 0
 
       t.timestamps
     end

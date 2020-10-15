86b13a391876c3054d921be99f16e55b109a5511
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-10-15 19:55:49 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+  root_to "home#top"
+  get "/about" => "home#about"
   devise_for :customers
   devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#お客様
+  resource :customer,only: [:show]
+  resource :cart_item
+  resource :delivery
+  resources :item
+  resource :order_item
+  resource :order
+
+#管理者
+  namespace :admin do
+    resources :orders
+    resources :genres
+    resources :items
+    resources :customer
+  end
 end

64bf717e1b376e5aa704fae1e8893f98abac7b94
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-10-19 15:31:24 +0900

    session

--- app/controllers/customer_devises/sessions_controller.rb
+++ app/controllers/customer_devises/sessions_controller.rb
@@ -4,21 +4,32 @@ class CustomerDevises::SessionsController < Devise::SessionsController
    before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-   def new
+  # def new
   #   super
-   end
+  # end
 
   # POST /resource/sign_in
-   def create
+  # def create
   #   super
-   end
+  # end
 
   # DELETE /resource/sign_out
-   def destroy
+  # def destroy
   #   super
+  # end
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "ログインに成功しました。"
+    root_path
+  end
+  def after_sign_out_path_for(resource)
+    flash[:notice] = "ログアウトに成功しました。"
+    root_path
+  end
+  def configure_account_update_params
+     devise_parameter_sanitizer.permit(:account_update, keys: [:password])
    end
 
-   protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_in_params

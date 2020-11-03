233840f2ebbf2fe938222f8701e312328eddd0a5
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-11-03 22:51:37 +0900

    genre/create

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -9,7 +9,21 @@ class Admin::GenresController < ApplicationController
     @genre = Genre.find{params[:id]}
   end
 
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to :index
+    else
+      render_to :index
+    end
+  end
+
   def update
     @genre.save
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name,:is_valid)
   end
 end

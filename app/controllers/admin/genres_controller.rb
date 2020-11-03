63e2d808f691249de8cc9101c0bbf221f0e1b837
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-11-03 23:06:56 +0900

    genre/update

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -20,7 +20,7 @@ class Admin::GenresController < ApplicationController
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
-      redirect_to admin_genre_path
+      redirect_to admin_genres_path
     else
       render :edit
     end

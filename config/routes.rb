77f6729d399e4da934047741a57ad506c9787a82
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-11-03 23:19:52 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,24 +10,23 @@ Rails.application.routes.draw do
     sessions: "admin_devises/sessions"
   }
 #お客様
-  namespace :customer do
-    resource :customer,only: [:show,:update] do
-      get "customer/emend" => "customers#emend"
-      get "customer/confirm" => "customers#confirm"
-      put "customer/hide" => "customers#hide"
-    end
-    resource :cart_item,only: [:index,:create,:update,:destroy] do
-      delete :destroy_all, on: :collection
-    end
-    resource :delivery,only: [:index,:edit,:create,:update,:destroy]
-    resources :items,only: [:show,:index]
-    resources :orders,only: [:new,:create,:index,:show] do
-      get :finish, on: :collection
-      get :confirm, on: :collection
-      post :confirm, on: :collection
-    end
-    patch "/order_items/:id" => "order_items#update"
+  resource :customer,only: [:show,:update]
+  get "customer/emend" => "customers#emend"
+  get "customer/confirm" => "customers#confirm"
+  put "customer/hide" => "customers#hide"
+
+  resource :cart_item,only: [:index,:create,:update,:destroy] do
+    delete :destroy_all, on: :collection
   end
+
+  resource :delivery,only: [:index,:edit,:create,:update,:destroy]
+  resources :items,only: [:show,:index]
+  resources :orders,only: [:new,:create,:index,:show] do
+    get :finish, on: :collection
+    get :confirm, on: :collection
+    post :confirm, on: :collection
+  end
+  patch "/order_items/:id" => "order_items#update"
 
 #管理者
   namespace :admin do
